008a1d95bc1b38ec184f03bb9fb75e9e05cbc445
Henri <henri.ducrocq@gmail.com>
2019-10-13 00:14:40 +0100

    eslint fixes

--- cephalo/app/cephalo.js
+++ cephalo/app/cephalo.js
@@ -15,83 +15,83 @@ const POD_PORT_RANGE_START = 3000
 const POD_PORT_RANGE_WIDTH = 10
 const POD_PATH_RX = new RegExp('^(/pod/(\\d+))(/.*)?$')
 const COOKIE_PATH_RX = /^(.* Path=)(.*)$/
-const CONTAINER_NAME_RX = /^p(\d+)$/
-const DOCKER_STATS_HEADERS = new RegExp("^CONTAINER ID +NAME +CPU % +MEM USAGE / LIMIT +MEM % +NET I/O +BLOCK I/O +PIDS$")
-const DOCKER_STATS_VALUES = new RegExp("^([^ ]+) +p([0-9]+) +([0-9.]+)% +([^ ]+) / ([^ ]+) +([^ ]+)% +([^ ]+) / ([^ ]+) +([^ ]+) / ([^ ]+) +([^ ]+)$")
+const DOCKER_STATS_HEADERS = new RegExp('^CONTAINER ID +NAME +CPU % +MEM USAGE / LIMIT +MEM % +NET I/O +BLOCK I/O +PIDS$')
+const DOCKER_STATS_VALUES = new RegExp('^([^ ]+) +p([0-9]+) +([0-9.]+)% +([^ ]+) / ([^ ]+) +([^ ]+)% +([^ ]+) / ([^ ]+) +([^ ]+) / ([^ ]+) +([^ ]+)$')
 
 const CONFIG = (() => {
   return {
     hostname: os.hostname(),
-    baseUrl: `http://localhost:${PORT}`,
-}})()
+    baseUrl: `http://localhost:${PORT}`
+  }
+})()
 
-//////////////////////////////// rest
+/// ///////////////////////////// rest
 
 var podStatsByNumber
 
 // regularly updates podStats object
-function collectPodsStats() {
-    const lines = proc.spawnSync('docker', ['stats', '--no-stream'])
-      .stdout.toString().trim().split('\n')
-    if (!DOCKER_STATS_HEADERS.test(lines[0])) {
-      console.error(`unexpected docker stats output: ${lines[0]}`)
+function collectPodsStats () {
+  const lines = proc.spawnSync('docker', ['stats', '--no-stream'])
+    .stdout.toString().trim().split('\n')
+  if (!DOCKER_STATS_HEADERS.test(lines[0])) {
+    console.error(`unexpected docker stats output: ${lines[0]}`)
+    return {}
+  }
+  const stats = {}
+  lines.slice(1).forEach(line => {
+    const matches = DOCKER_STATS_VALUES.exec(line)
+    if (!matches) {
+      console.error(`unexpected docker stats output: ${line}`)
       return {}
     }
-    const stats = {}
-    lines.slice(1).forEach(line => {
-      const matches = DOCKER_STATS_VALUES.exec(line)
-      if (!matches) {
-        console.error(`unexpected docker stats output: ${line}`)
-        return {}
-      }
 
-      let [all, containerId, podNumber, cpu, memUsage, memLimit, memPercent,
-           netIn, netOut, blockIn, blockOut, pids] = matches
-      const metrics = {
-        containerId: containerId,
-        podNumber: parseInt(podNumber),
-        cpu: cpu,
-        memUsage: memUsage,
-        memLimit: memLimit,
-        memPercent: memPercent,
-        netIn: netIn,
-        netOut: netOut,
-        blockIn: blockIn,
-        blockOut: blockOut,
-        pids: pids
-      }
+    const [, containerId, podNumber, cpu, memUsage, memLimit, memPercent,
+      netIn, netOut, blockIn, blockOut, pids] = matches
+    const metrics = {
+      containerId: containerId,
+      podNumber: parseInt(podNumber),
+      cpu: cpu,
+      memUsage: memUsage,
+      memLimit: memLimit,
+      memPercent: memPercent,
+      netIn: netIn,
+      netOut: netOut,
+      blockIn: blockIn,
+      blockOut: blockOut,
+      pids: pids
+    }
 
-      let statusUrl = `${CONFIG.baseUrl}/pod/${podNumber}/status`
-      request({
-          url: statusUrl,
-        },
-        (err, res, body) => {
-          if (err) {
-            console.error(`error getting ${statusUrl}`, err)
-            return
-          }
-          const podStatus = JSON.parse(body)
-          podStatus['metrics'] = metrics
-          stats[parseInt(podNumber)] = podStatus
-        })
+    const statusUrl = `${CONFIG.baseUrl}/pod/${podNumber}/status`
+    request({
+      url: statusUrl
+    },
+    (err, res, body) => {
+      if (err) {
+        console.error(`error getting ${statusUrl}`, err)
+        return
+      }
+      const podStatus = JSON.parse(body)
+      podStatus.metrics = metrics
+      stats[parseInt(podNumber)] = podStatus
     })
-    podStatsByNumber = stats
-    setTimeout(collectPodsStats, 10000)
+  })
+  podStatsByNumber = stats
+  setTimeout(collectPodsStats, 10000)
 }
 collectPodsStats()
 
-function restPodStats(req, res) {
+function restPodStats (req, res) {
   const podNumber = req.params.podNumber
   res.setHeader('Content-type', 'application/json')
   res.end(JSON.stringify(podStatsByNumber[podNumber]))
 }
 
-function restStats(req, res) {
-    res.setHeader('Content-type', 'application/json')
-    res.end(JSON.stringify(podStatsByNumber))
+function restStats (req, res) {
+  res.setHeader('Content-type', 'application/json')
+  res.end(JSON.stringify(podStatsByNumber))
 }
 
-function nextPodNumber() {
+function nextPodNumber () {
   // FIXME should be persistent, cluster-wide
   const NEXT_POD_NUMBER_PATH = '/tmp/next-pod-number'
   const num = parseInt(fs.readFileSync(NEXT_POD_NUMBER_PATH))
@@ -99,7 +99,7 @@ function nextPodNumber() {
   return num
 }
 
-function restCreate(req, res) {
+function restCreate (req, res) {
   const homedir = req.body.homedir
   const projects = req.body.projects
   const user = '43880338'
@@ -111,10 +111,10 @@ function restCreate(req, res) {
     `${podNumber}`,
     'wetty',
     user,
-    userDisplayName,
+    userDisplayName
   ]
-  .concat([homedir])
-  .concat(projects)
+    .concat([homedir])
+    .concat(projects)
   console.log(args)
   proc.spawn('./start-pod', args)
 
@@ -122,13 +122,13 @@ function restCreate(req, res) {
   res.end(JSON.stringify(`${CONFIG.baseUrl}/pod/${podNumber}/`))
 }
 
-//////////////////////////////// proxy to pod
+/// ///////////////////////////// proxy to pod
 
-function podPort(podNumber) {
+function podPort (podNumber) {
   return POD_PORT_RANGE_START + podNumber * POD_PORT_RANGE_WIDTH
 }
 
-function podBasePath(url) {
+function podBasePath (url) {
   const matches = POD_PATH_RX.exec(url)
   if (!matches) return undefined
   return `/pod/${matches[2]}`
@@ -153,7 +153,6 @@ const theProxy = proxy({
     if (!matches) return `http://127.0.0.1:${PORT}` // go home
 
     const number = parseInt(matches[2])
-    const path = matches[3] || ''
     const port = podPort(number) + 1
     const target = `http://127.0.0.1:${port}`
     return target
@@ -179,9 +178,9 @@ const theProxy = proxy({
 
 app.use(POD_PATH_RX, theProxy)
 app.use('/', express.static(path.join(__dirname, 'public')))
-//app.use(function (req, res, next) {
+// app.use(function (req, res, next) {
 //    res.status(404).send('not found')
-//})
+// })
 app.use(bodyParser.json())
 app.get('/stats/:podNumber', restPodStats)
 app.get('/stats', restStats)
